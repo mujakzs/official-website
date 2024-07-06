c4870780c3cacd82186e91fd03b0fe2f5bee749b
mujakzs <andriangultiano14@gmail.com>
2024-07-06 16:22:54 +0800

    Webportfolio

---
+++ js/galleries.js
@@ -0,0 +1,287 @@
+// GALLERY 1
+jQuery('#gallery1').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/a.jpg',
+            'thumb': 'images/photos/a.jpg',
+            'subHtml': "Andrian THe great"
+        }, {
+            'src': 'images/photos/c1.jpg',
+            'thumb': 'images/photos/c1.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c2.jpg',
+            'thumb': 'images/photos/c2.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c3.jpg',
+            'thumb': 'images/photos/c3.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c4.jpg',
+            'thumb': 'images/photos/c4.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c5.jpg',
+            'thumb': 'images/photos/c5.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c6.jpg',
+            'thumb': 'images/photos/c6.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c7.jpg',
+            'thumb': 'images/photos/c7.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c8.jpg',
+            'thumb': 'images/photos/c8.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c9.jpg',
+            'thumb': 'images/photos/c9.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c10.jpg',
+            'thumb': 'images/photos/c10.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c11.jpg',
+            'thumb': 'images/photos/c11.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/c12.jpg',
+            'thumb': 'images/photos/c12.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/martronix.jpg',
+            'thumb': 'images/photos/martronix.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/martronix3.jpg',
+            'thumb': 'images/photos/martronix3.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/d2.jpg',
+            'thumb': 'images/photos/d2.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/d3.jpg',
+            'thumb': 'images/photos/d3.jpg',
+            'subHtml': "muj"
+        }
+		
+		]
+    })
+});
+
+// GALLERY 2
+jQuery('#gallery2').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/d.jpg',
+            'thumb': 'images/photos/d.jpg',
+            'subHtml': 'CPE'
+        }, {
+            'src': 'images/photos/d1.jpg',
+            'thumb': 'images/photos/d1.jpg',
+            'subHtml': "Tribute to parents"
+        }, {
+            'src': 'images/photos/d4.jpg',
+            'thumb': 'images/photos/d4.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/d6.jpg',
+            'thumb': 'images/photos/d6.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/genpack2.jpg',
+            'thumb': 'images/photos/genpack2.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/caap.jpg',
+            'thumb': 'images/photos/caap.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/f3.jpg',
+            'thumb': 'images/photos/f3.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/f4.jpg',
+            'thumb': 'images/photos/f4.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/f1.jpg',
+            'thumb': 'images/photos/f1.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/f2.jpg',
+            'thumb': 'images/photos/f2.jpg',
+            'subHtml': "muj"
+        }
+        
+       
+		]
+    })
+});
+
+// GALLERY 3
+jQuery('#gallery3').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/a11.jpg',
+            'thumb': 'images/photos/a1.jpg',
+            'subHtml': 'with maam '
+        }, {
+            'src': 'images/photos/abc.jpg',
+            'thumb': 'images/photos/abc.jpg',
+            'subHtml': "Andrian Gultiano"
+        }, {
+            'src': 'images/photos/b.jpg',
+            'thumb': 'images/photos/b.jpg',
+            'subHtml': "chuy"
+        }, {
+            'src': 'images/photos/a4.jpg',
+            'thumb': 'images/photos/a4.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/a5.jpg',
+            'thumb': 'images/photos/a5.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/a6.jpg',
+            'thumb': 'images/photos/a6.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/a7.jpg',
+            'thumb': 'images/photos/a7.jpg',
+            'subHtml': "muj"
+        }
+		]
+    })
+});
+
+// GALLERY 4
+jQuery('#gallery4').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/s1.jpg',
+            'thumb': 'images/photos/s1.jpg',
+            'subHtml': 'huhuhuhuhuh'
+        }, {
+            'src': 'images/photos/s2.jpg',
+            'thumb': 'images/photos/s2.jpg',
+            'subHtml': "hehehehehe"
+        }, {
+            'src': 'images/photos/s3.jpg',
+            'thumb': 'images/photos/s3.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/s4.jpg',
+            'thumb': 'images/photos/s4.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s5.jpg',
+            'thumb': 'images/photos/s5.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s6.jpg',
+            'thumb': 'images/photos/s6.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s7.jpg',
+            'thumb': 'images/photos/s7.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s8.jpg',
+            'thumb': 'images/photos/s8.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s9.jpg',
+            'thumb': 'images/photos/s9.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s10.jpg',
+            'thumb': 'images/photos/s10.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s11.jpg',
+            'thumb': 'images/photos/s11.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s12.jpg',
+            'thumb': 'images/photos/s12.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s13.jpg',
+            'thumb': 'images/photos/s13.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s14.jpg',
+            'thumb': 'images/photos/s14.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s15.jpg',
+            'thumb': 'images/photos/s15.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s16.jpg',
+            'thumb': 'images/photos/s16.jpg',
+            'subHtml': "muj"
+        }, {
+            'src': 'images/photos/s17.jpg',
+            'thumb': 'images/photos/s17.jpg',
+            'subHtml': "muj"
+        
+        }
+		]
+    })
+});
